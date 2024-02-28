c392e3eab49d349e45264ead69ae7c1f58f7689c
NINGAYYA MATHAPATI <ningayyamathapati2000@gmail.com>
2024-02-28 17:20:11 +0530

    recent

---
+++ PropelEdtech/src/Components/Blackhole.jsx
@@ -0,0 +1,9 @@
+export default function Blackhole(){
+    return(
+        <>
+        <canvas class="experience"></canvas>
+        <div class="scrollTarget"></div>
+        <div class="vignette-radial"></div>
+        </>
+    );
+}
--- PropelEdtech/src/Components/LandingComponent.jsx
+++ PropelEdtech/src/Components/LandingComponent.jsx
@@ -87,3 +87,4 @@ export default function LandingComponent() {
               <AnimatedButton></AnimatedButton>
               </div> */
 }
+e
